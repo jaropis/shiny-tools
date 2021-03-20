c685aaf7337992bc5e720c6bd34879b4196fba03
Jarosław Piskorski <jaropis@zg.home.pl>
2021-03-20 10:36:17 +0100

    percentuj doc changed

--- R/procentuj.R
+++ R/procentuj.R
@@ -1,7 +1,7 @@
 #' this function takes a data.frame column with proportions (possibly from prop.table) and returns a formatted  column with percentages, including the % sign
 #' 
-#' @param accepts a data.frame column or a vector, whether to include the % symbol and whether there should be a space between the number and the % symbol
-#' @return returns a formatted string column or a string vector with the % sign
+#' @param accepts a data.frame column or a vector, whether to include the percent symbol and whether there should be a space between the number and the % symbol
+#' @return returns a formatted string column or a string vector with the percent sign
 #' @export
 procentuj <- function(column, percent_symbol=TRUE, round_to=2, sep=" "){
   return(paste(round(column, round_to),c("%"), sep=sep))
