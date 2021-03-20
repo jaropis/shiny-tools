4a7e0063f31d4f452120a9cf49acb16be95b3ef9
Jarosław Piskorski <jaropis@zg.home.pl>
2021-03-20 10:42:05 +0100

    percent is back in code

--- R/procentuj.R
+++ R/procentuj.R
@@ -4,5 +4,5 @@
 #' @return returns a formatted string column or a string vector with the percent sign
 #' @export
 procentuj <- function(column, percent_symbol=TRUE, round_to=2, sep=" "){
-  return(paste(round(column, round_to),c(""), sep=sep))
+  return(paste(round(column, round_to),c("%"), sep=sep))
 }
